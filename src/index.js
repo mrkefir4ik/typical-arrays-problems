e8ffc7e7786ee8e0ed58bb55b81a6a6e13297fa9
mrkefir4ik <joker19980621@gmail.com>
2020-10-01 19:50:44 +0300

    feat: implement all functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0
+  }
+  else{
+    let result = array[0];
+    for(let i =0; i < array.length; i++){
+      if (array[i] < result) {
+        result = array[i];
+      }
+    }
+    return result;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0
+  }
+  else{
+    let result = array[0];
+    for(let i =0; i < array.length; i++){
+      if (array[i] > result) {
+        result = array[i];
+      }
+    }
+    return result;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0){
+    return 0
+  }
+  else {
+    let result = 0;
+    for (let i = 0; i < array.length; i++){
+    result += array[i];
+  }
+  return result / array.length;
+}
 }
